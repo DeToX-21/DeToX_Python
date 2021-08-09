560807e59885823fc2635f27ee90770dfff0803a
DeToX-21 <78383351+DeToX-21@users.noreply.github.com>
2021-08-09 20:14:23 +0200

    Add files via upload

---
+++ ZCasino.py
@@ -0,0 +1,49 @@
+""" Ce programme simule un jeu simplifé de la roulette."""
+
+# Règle du jeu
+# On choisit un numéro entre 0 et 49
+# On place une mise
+# Si numéro gagnant --> On gagne 3 fois la mise
+# Si notre numéro et numéro gagnant paire ou impaire --> On récupère 50% de la mise
+# Sinon on ne gagne rien
+# Bonus: On peut jouer tant qu'il y'a de l'argent dans notre porte-monnaie (wallet).
+
+from random import randrange
+from math import ceil
+
+wallet = 1000
+
+while wallet > 0:
+
+    number = int(input("Entrez un numéro entre 0 et 49:"))
+
+    while number < 0 or number >= 50:
+        print("Votre numéro n'est pas compris entre 0 et 49.")
+        number = int(input("Entrez un numéro entre 0 et 49:"))
+
+    bet = int(input("Entrez le montant de votre mise:"))
+
+    while bet > wallet:
+        print("Vous devez entrer une mise inférieur à {}$!".format(wallet))
+        bet = int(input("Entrez le montant de votre mise:"))
+
+    wallet -= bet
+
+    winning_number = randrange(50)
+    print("Le numéro gagnant est le {}!".format(winning_number))
+
+    if number == winning_number:
+        price = 3 * bet
+        wallet += price
+        print("Vous venez de gagner {}$!".format(price))
+    elif number % 2 == winning_number % 2 == 0 or (number % 2 and winning_number % 2) != 0:
+        price = ceil(0.5 * bet)
+        wallet += price
+        print("Vous récupérez {}$.".format(price))
+    else:
+        print("Vous avez perdu!!!")
+
+    print("Il vous reste {}$ dans votre porte monnaie.".format(wallet))
+else:
+    print("Vous êtes fauché. GAME OVER !!!")
+    quit()
