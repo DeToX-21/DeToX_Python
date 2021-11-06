8e23ded7c8c2a24adf48f4f65ce762271e11afd0
M_Zakaria <78383351+M-Zakaria-git@users.noreply.github.com>
2021-11-06 22:19:39 +0100

    Add files via upload

---
+++ donnees.py
@@ -0,0 +1,27 @@
+
+# Nombre de tentatives
+chances = 8
+
+# Liste de mots a trouver dans le pendu.
+liste_mots = [
+    "armoire",
+    "boucle",
+    "buisson",
+    "bureau",
+    "chaise",
+    "carton",
+    "couteau",
+    "fichier",
+    "garage",
+    "glace",
+    "journal",
+    "kiwi",
+    "lampe",
+    "liste",
+    "montagne",
+    "remise",
+    "sandale",
+    "taxi",
+    "vampire",
+    "volant",
+]
